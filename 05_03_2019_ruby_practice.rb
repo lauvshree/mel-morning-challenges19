fe3370d99a6aea870e1e3db502f4a602bbef96f6
Anhar Athoi <anharzihan@gmail.com>
2019-03-05 07:56:14 +1100

    Added morning challenge for 05/03/2019- ruby revision

---
+++ 05_03_2019_ruby_practice.rb
@@ -0,0 +1,82 @@
+# Ruby revision quiz 
+
+# INSTRUCTIONS: Type an answer for each of the questions below without running code.
+# Once you have completed all the answers run the code to verify your answers
+
+
+# **Question 1:** What does the following code display?   
+# puts (2 + 4 * 5 - 10)
+# Your Answer:  
+# Your verified answer:
+
+# **Question 2:** What does the following code display?   
+# puts ((2 + 4) * 5 - 10)    
+# Your Answer:  
+# Your verified answer:
+
+# **Question 3:** What does the following code display?   
+# puts puts ((2 + 4) * (5 - 10))    
+# Your Answer:  
+# Your verified answer:
+
+# **Question 4:** What does the following code display?  
+# counter = 100 
+# puts (counter > 100 ? "not right" : "just right")
+# Your Answer:  
+# Your verified answer:
+
+# **Question 5:** What does the following code display?  
+# counter = 100 
+# if (counter > 100) then
+#     puts ("not right")
+# else
+#     puts ("just right") 
+# end
+# Your Answer:  
+# Your verified answer:
+
+# **Question 6:**  What does the following code display?  
+# arr = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+# puts(arr[1])
+# Your Answer:  
+# Your verified answer: 
+
+# **Question 7:** In Ruby, puts is a method?  
+# arr = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+# puts(arr[-1])
+# Your Answer:  
+# Your verified answer: 
+
+# **Question 8:** What will the following display?  
+# arr = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+# print(arr[3])
+# print(arr[4])
+# print(arr[5])
+# Your Answer:  
+# Your verified answer: 
+ 
+# **Question 9:** What will the following display?  
+# coniditon = "true"
+# puts(coniditon.class)
+# Your Answer:  
+# Your verified answer:
+  
+# **Question 10:**  What will the following display?   
+# value = 10.0
+# puts(value.class)
+# Your Answer:  
+# Your verified answer:
+
+# **Question 11:**  What will the following display?   
+# value = 10
+# colors = ["blue", "green", "red"]
+# puts("#{value}#{colors[1]}")
+# Your Answer:  
+# Your verified answer:
+
+# **Question 12:**  What will the following display?   
+# value = 10
+# colors = ["blue", "green", "red"]
+# value < 20 ? puts("#{value} #{colors[0]}") : puts("#{colors[0]}")
+# Your Answer:  
+# Your verified answer:
