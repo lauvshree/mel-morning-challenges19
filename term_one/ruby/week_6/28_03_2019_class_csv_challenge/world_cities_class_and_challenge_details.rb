3179e8f89c38accc9af6384d8f3f47759d033802
Harrison Malone <harrison.malone2@gmail.com>
2019-03-28 22:36:29 +1100

    name change to file structure

--- term_one/ruby/week_6/28_03_2019_class_csv_challenge/world_cities_class_and_challenge_details.rb
+++ term_one/ruby/week_6/28_03_2019_class_csv_challenge/world_cities_class_and_challenge_details.rb
@@ -1,6 +1,7 @@
-require "csv"
-require "pry"
+require 'csv'
+require 'pry'
 
+# city class with name, country and subcountry as attributes
 class City
   attr_reader :name, :country, :subcountry
 
@@ -11,35 +12,36 @@ class City
   end
 end
 
-csv_text = File.read("world-cities.csv")
-csv = CSV.parse(csv_text, :headers => true)
+csv_text = File.read('world-cities.csv')
+csv = CSV.parse(csv_text, headers: true)
 cities = []
 csv.each do |row|
   row_data = row.to_hash
 
   puts row_data
-  cities << City.new(row_data["name"], row_data["country"], row_data["subcountry"])
+  cities << City.new(row_data['name'], row_data['country'],
+                     row_data['subcountry'])
 end
 
 def subcountry_city_count(cities, country, subcountry)
   count = 0
-  cities.each do |city|
-    if (city.country == country && city.subcountry == subcountry)
-      count = count + 1
-    end
-  end
-
-  return count
+  # cities.each do |city|
+  #   count += 1 if city.country == country && city.subcountry == subcountry
+  # end
+  # count
+  count = cities.select do |city|
+    city.country == country && city.subcountry == subcountry
+  end 
+  p count.sum
 end
 
-city_count = subcountry_city_count(cities, "Australia", "Victoria")
-print "ERROR!!! " if city_count != 90
+city_count = subcountry_city_count(cities, 'Australia', 'Victoria')
+print 'ERROR!!! ' if city_count != 90
 puts("Expect Victoria, Australia to have 90 cities and got #{city_count}")
 
-city_count = subcountry_city_count(cities, "United States", "Florida")
-print "ERROR!!! " if city_count != 227
+city_count = subcountry_city_count(cities, 'United States', 'Florida')
+print 'ERROR!!! ' if city_count != 227
 puts("Expect Florida, United States to have 227 and got #{city_count}")
-
 
 # 0. Run and verify it works
 
